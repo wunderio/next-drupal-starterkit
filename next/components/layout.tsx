2f4a7dbf4eaef0b796b27ce296bad013586007f2
Mario Vercellotti <mario.vercellotti@wunder.io>
2022-11-23 14:39:32 +0200

    NEX-18: Add displaying of menus in the example next app. Improve language switcher and utils

--- next/components/layout.tsx
+++ next/components/layout.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
+import { DrupalMenuLinkContent } from "next-drupal";
+import { Footer } from "components/footer";
+import { Navbar } from "components/navbar";
 
-import LocaleSwitcher from "../components/locale-switcher";
 import { PreviewAlert } from "../components/preview-alert";
 
-export function Layout({ children }) {
+export interface LayoutProps {
+  menus: {
+    main: DrupalMenuLinkContent[];
+    footer: DrupalMenuLinkContent[];
+  };
+  children?: React.ReactNode;
+}
+
+export function Layout({ menus, children }: LayoutProps) {
   return (
     <>
       <PreviewAlert />
       <div className="max-w-screen-md px-6 mx-auto">
-        <header>
-          <div className="container flex items-center justify-between py-6 mx-auto">
-            <Link href="/" passHref>
-              <a className="text-2xl font-semibold no-underline">
-                Next-drupal wunder template
-              </a>
-            </Link>
-            <LocaleSwitcher />
-          </div>
-        </header>
+        <Navbar links={menus.main} />
+
         <main className="container py-10 mx-auto">{children}</main>
+        <Footer links={menus.footer} />
       </div>
     </>
   );
--- next/lib/utils.ts
+++ next/lib/utils.ts
@@ -1,3 +1,8 @@
+import { GetStaticPropsContext } from "next";
+import { DrupalClient, DrupalNode } from "next-drupal";
+
+import siteConfig from "../site.config";
+
 export function formatDate(input: string): string {
   const date = new Date(input);
   return date.toLocaleDateString("en-US", {
@@ -10,3 +15,38 @@ export function formatDate(input: string): string {
 export function absoluteUrl(input: string) {
   return `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}${input}`;
 }
+
+/**
+ * Gets the language links for the language switcher.
+ */
+export const setLanguageLinks = (translations = []) => {
+  const languageLinks = JSON.parse(JSON.stringify(siteConfig.locales));
+  Object.entries(translations).forEach((translation) => {
+    languageLinks[translation[0]].path = translation[1];
+  });
+  return languageLinks;
+};
+
+/**
+ * Given a node, it will return the node translations for it.
+ */
+export const getNodeTranslatedVersions = async (
+  node: DrupalNode,
+  context: GetStaticPropsContext,
+  drupal: DrupalClient
+) => {
+  const nodeTranslations = {};
+  for (let i = 0; i < context.locales.length; i++) {
+    const lang = context.locales[i];
+    const translated = await drupal.getResource(node.type, node.id, {
+      locale: lang,
+      defaultLocale: context.defaultLocale,
+      withAuth: true,
+    });
+    // Only consider a translation if there is actually a translated
+    // node:
+    nodeTranslations[translated.langcode] = translated.path.alias;
+  }
+
+  return nodeTranslations;
+};
