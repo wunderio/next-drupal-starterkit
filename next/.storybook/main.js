2c17074e59929855833d63c707ec5cd666017357
Mario Vercellotti <mario.vercellotti@wunder.io>
2022-12-09 11:22:55 +0200

    NEX-25: more fixes for storybook

--- next/.storybook/main.js
+++ next/.storybook/main.js
@@ -1,4 +1,5 @@
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   stories: [
@@ -9,6 +10,18 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
   framework: "@storybook/react",
   core: {
@@ -25,6 +38,14 @@ module.exports = {
       ...config.resolve.alias,
       "next-i18next": "react-i18next",
     };
+    /**
+     * Fixes font import with /
+     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
+     */
+    config.resolve.roots = [
+      path.resolve(__dirname, "../public"),
+      "node_modules",
+    ];
     return config;
   },
 };
