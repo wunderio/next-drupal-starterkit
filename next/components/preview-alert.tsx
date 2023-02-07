f0defbae2848000128d72f9cd7d8d31cc063713e
Joshua Scott <git@joshuascott.io>
2023-02-07 17:08:26 +0200

    NEX-33: Temporarily hide PreviewAlert while styling layout

--- next/components/preview-alert.tsx
+++ next/components/preview-alert.tsx
@@ -13,16 +13,18 @@ export function PreviewAlert() {
     return null;
   }
 
-  return (
-    <div className="sticky top-0 left-0 z-50 w-full bg-black px-2 py-1 text-center text-white">
-      <p className="mb-0">
-        This page is a preview.{" "}
-        {/* eslint-disable @next/next/no-html-link-for-pages */}
-        <a href="/api/exit-preview" className="text-white underline">
-          Click here
-        </a>{" "}
-        to exit preview mode.
-      </p>
-    </div>
-  );
+  // todo: reinstate this
+  return null;
+  // return (
+  //   <div className="sticky top-0 left-0 z-50 w-full bg-black px-2 py-1 text-center text-white">
+  //     <p className="mb-0">
+  //       This page is a preview.{" "}
+  //       {/* eslint-disable @next/next/no-html-link-for-pages */}
+  //       <a href="/api/exit-preview" className="text-white underline">
+  //         Click here
+  //       </a>{" "}
+  //       to exit preview mode.
+  //     </p>
+  //   </div>
+  // );
 }
