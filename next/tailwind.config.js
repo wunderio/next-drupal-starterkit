0aecb9a7171e4278e6ff33b7ed795c6b5f326db0
Joshua Scott <git@joshuascott.io>
2023-02-13 17:13:43 +0200

    NEX-28: Add component library font sizes and line heights

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -11,10 +11,29 @@ module.exports = {
     fontFamily: {
       sans: ["var(--font-overpass)", ...fontFamily.sans],
     },
+    fontSize: {
+      xs: ["0.60rem", { lineHeight: "1.5" }],
+      sm: ["0.80rem", { lineHeight: "1.5" }],
+      md: ["1.00rem", { lineHeight: "1.5" }],
+      lg: ["1.25rem", { lineHeight: "1.5" }],
+      xl: ["1.60rem", { lineHeight: "1.5" }],
+      "2xl": ["1.9rem", { lineHeight: "1.5" }],
+      "heading-xs": ["1.25rem", { lineHeight: "1.25" }],
+      "heading-sm": ["1.9rem", { lineHeight: "1.25" }],
+      "heading-md": ["2.4rem", { lineHeight: "1.25" }],
+      "heading-lg": ["3.1rem", { lineHeight: "1.25" }],
+      "heading-xl": ["3.8rem", { lineHeight: "1.25" }],
+      "heading-2xl": ["4.75rem", { lineHeight: "1.25" }],
+    },
     fontWeight: {
       light: "300",
       regular: "400",
       bold: "700",
+    },
+    lineHeight: {
+      md: "1.25",
+      lg: "1.5",
+      xl: "1.75",
     },
     // These properties *extend* the defaults:
     extend: {
