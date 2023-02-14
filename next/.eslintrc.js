5bd43ae37c4aa289b62433c416a5d54f3377a278
Joshua Scott <git@joshuascott.io>
2023-02-14 17:20:37 +0200

    Add @typescript-eslint/recommended-requiring-type-checking

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
       extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "plugin:@next/next/recommended",
         "prettier",
       ],
