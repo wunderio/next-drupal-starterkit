6b926ff4a7558f99abbf084074d484b5494da3ba
Joshua Scott <git@joshuascott.io>
2023-02-14 17:20:50 +0200

    Relax typescript rules further

--- next/.eslintrc.js
+++ next/.eslintrc.js
@@ -49,7 +49,12 @@ module.exports = {
         "@typescript-eslint/ban-ts-comment": "off",
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
         "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
 
         // Sort imports.
         "simple-import-sort/imports": [
