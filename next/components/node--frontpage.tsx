7e5825269529a379b121d61dfe34030d549b72bc
Mario Vercellotti <mario.vercellotti@wunder.io>
2023-02-20 14:52:58 +0200

    NEX-43: Do not break the build script if there is no frontpage node created
    * this happens during the "megacommand" on installation for example

--- next/components/node--frontpage.tsx
+++ next/components/node--frontpage.tsx
@@ -7,7 +7,7 @@ interface NodeFrontPageProps {
 }
 
 export function NodeFrontpage({ node }: NodeFrontPageProps) {
-  if (!node.field_content_elements.length) return null;
+  if (!node?.field_content_elements?.length) return null;
 
   return node.field_content_elements.map((paragraph: DrupalParagraph) => {
     return <Paragraph key={paragraph.id} paragraph={paragraph} />;
