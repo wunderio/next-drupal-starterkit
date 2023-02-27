5f85c90c734a2909999c3586fcd16883bc7a9d80
Mario Vercellotti <mario.vercellotti@wunder.io>
2023-02-27 09:30:13 +0200

    NEX-44: Improve form handling with react-hook-form
    * Show a confirmation after submit
    * add button to submit again

--- next/components/contact-form.tsx
+++ next/components/contact-form.tsx
@@ -1,90 +1,121 @@
 import { useTranslation } from "next-i18next";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 export function ContactForm() {
   const { t } = useTranslation();
-  async function handleSubmit(event) {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitted },
+  } = useForm<Inputs>();
+
+  const onSubmit = async (data) => {
     const response = await fetch(`/api/contact`, {
       method: "POST",
       body: JSON.stringify({
-        name: event.target.name.value,
-        email: event.target.email.value,
-        message: event.target.message.value,
-        subject: event.target.subject.value,
+        name: data.name,
+        email: data.email,
+        message: data.message,
+        subject: data.subject,
       }),
     });
 
-    if (response.ok) {
-      // TODO: show some visible feedback?
-      alert("Thanks!");
+    if (!response.ok) {
+      alert("Error!");
     }
-
-    // TODO: Handle error.
-  }
+  };
 
   return (
-    <form
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      onSubmit={handleSubmit}
-      className="mb-4 flex flex-col gap-5 rounded border bg-white p-4 shadow-md transition-all hover:shadow-md"
-    >
-      <h2 className="text-heading-sm font-bold md:text-heading-md">
-        {t("form-title")}
-      </h2>
-      <p>{t("form-description")}</p>
-      <div>
-        <label className="mb-2 block text-sm font-bold" htmlFor="name">
-          {t("form-label-name")}
-        </label>
-        <input
-          className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3  shadow "
-          type="text"
-          id="name"
-          name="name"
-          required
-        />
-      </div>
-      <div>
-        <label className="mb-2 block text-sm font-bold" htmlFor="email">
-          {t("form-label-email")}
-        </label>
-        <input
-          className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
-          type="email"
-          id="email"
-          name="email"
-          required
-        />
-      </div>
-      <div>
-        <label className="mb-2 block text-sm font-bold" htmlFor="subject">
-          {t("form-label-subject")}
-        </label>
-        <input
-          className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
-          type="text"
-          id="subject"
-          name="subject"
-        />
-      </div>
-      <div>
-        <label className="mb-2 block text-sm font-bold" htmlFor="message">
-          {t("form-label-message")}
-        </label>
-        <textarea
-          className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
-          id="message"
-          name="message"
-          required
-        />
-      </div>
+    <div className="mb-4 rounded border bg-white p-4 shadow-md transition-all hover:shadow-md">
+      {isSubmitted && (
+        <div
+          className="border-l-4 border-wunderpurple-500 bg-wunderpurple-100 p-4 text-wunderpurple-700"
+          role="alert"
+        >
+          <p className="mb-3 text-xl font-bold">
+            {t("form-thank-you-message")}
+          </p>
+          <button
+            className="rounded bg-wunderpurple-500 py-2 px-4 font-bold text-white hover:bg-wunderpurple-700"
+            onClick={() => reset()}
+          >
+            {t("form-send-another-message")}
+          </button>
+        </div>
+      )}
+      {!isSubmitted && (
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+          <h2 className="text-heading-sm font-bold md:text-heading-md">
+            {t("form-title")}
+          </h2>
+          <p>{t("form-description")}</p>
+          <div>
+            <label className="mb-2 block text-sm font-bold" htmlFor="name">
+              {t("form-label-name")}
+            </label>
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3  shadow "
+              type="text"
+              id="name"
+              name="name"
+              required
+              {...register("name")}
+            />
+          </div>
+          <div>
+            <label className="mb-2 block text-sm font-bold" htmlFor="email">
+              {t("form-label-email")}
+            </label>
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
+              type="email"
+              id="email"
+              name="email"
+              {...register("email")}
+              required
+            />
+          </div>
+          <div>
+            <label className="mb-2 block text-sm font-bold" htmlFor="subject">
+              {t("form-label-subject")}
+            </label>
+            <input
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
+              type="text"
+              id="subject"
+              name="subject"
+              {...register("subject")}
+            />
+          </div>
+          <div>
+            <label className="mb-2 block text-sm font-bold" htmlFor="message">
+              {t("form-label-message")}
+            </label>
+            <textarea
+              className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 shadow"
+              id="message"
+              name="message"
+              {...register("message")}
+              required
+            />
+          </div>
 
-      <button
-        className="focus:shadow-outline rounded bg-wunderpurple-500 py-2 px-4 font-bold text-white hover:bg-wunderpurple-700"
-        type="submit"
-      >
-        {t("form-submit")}
-      </button>
-    </form>
+          <button
+            className="focus:shadow-outline rounded bg-wunderpurple-500 py-2 px-4 font-bold text-white hover:bg-wunderpurple-700"
+            type="submit"
+          >
+            {t("form-submit")}
+          </button>
+        </form>
+      )}
+    </div>
   );
 }
