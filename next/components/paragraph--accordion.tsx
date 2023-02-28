fa5107a8e1427e6c0e16f262232088fca92cf6e1
Mario Vercellotti <mario.vercellotti@wunder.io>
2023-02-28 12:56:42 +0200

    NEX-45: Add migration for the accordion and accordion_items nested paragraph
    * also, improved migration of other paragraph types

---
+++ next/components/paragraph--accordion.tsx
@@ -0,0 +1,5 @@
+import { Accordion } from "@/lib/zod/paragraph";
+
+export function ParagraphAccordion({ paragraph }: { paragraph: Accordion }) {
+  return <div>Accordion paragraph placeholder. Id:{paragraph.id}</div>;
+}
