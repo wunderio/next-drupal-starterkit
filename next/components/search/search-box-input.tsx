6038e0a99b4be1ee1da632a0962e8da9ab5a1eea
Joshua Scott <git@joshuascott.io>
2023-04-12 16:57:03 +0300

    NEX-48: Search page is now responsive

--- next/components/search/search-box-input.tsx
+++ next/components/search/search-box-input.tsx
@@ -11,14 +11,11 @@ export const SearchBoxInput: React.ComponentType<InputViewProps> = ({
   return (
     <div className="relative w-full max-w-xl">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <SearchIcon
-          aria-hidden
-          className="h-5 w-5 text-scapaflow dark:text-stone"
-        />
+        <SearchIcon aria-hidden className="h-5 w-5 text-scapaflow" />
       </div>
       <input
         {...getInputProps()}
-        className="block w-full rounded-lg border border-primary-100 bg-white p-4 pl-10 text-sm text-steelgray focus:border-primary-500 focus:ring-primary-500 dark:border-scapaflow dark:bg-steelgray dark:text-white dark:placeholder-graysuit dark:focus:border-primary-500 dark:focus:ring-primary-500"
+        className="block w-full rounded-lg border border-primary-100 bg-white p-4 pl-10 text-sm text-steelgray focus:border-primary-500 focus:ring-primary-500"
         placeholder={t("search-bar-placeholder-text")}
       />
       <button
