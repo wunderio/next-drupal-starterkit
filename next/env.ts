8c34976486f9e34701a1ed6e5dc12b3782000ac0
Joshua Scott <git@joshuascott.io>
2023-04-26 14:15:54 +0300

    Use @t3-oss/env-nextjs for typesafe env vars

--- next/env.ts
+++ next/env.ts
@@ -1,28 +1,23 @@
+import { createEnv } from "@t3-oss/env-nextjs";
 import zod from "zod";
 
-const envSchema = zod.object({
-  // These are only available on the server:
-  DRUPAL_CLIENT_ID: zod.string().optional(),
-  DRUPAL_CLIENT_SECRET: zod.string().optional(),
-  DRUPAL_REVALIDATE_SECRET: zod.string().optional(),
-  NEXT_IMAGE_DOMAIN: zod.string().optional(),
-
-  // These are available on both server and client:
-  NODE_ENV: zod.union([
-    zod.literal("development"),
-    zod.literal("production"),
-    zod.literal("test"),
-  ]),
-  NEXT_PUBLIC_FRONTEND_URL: zod.string(),
-  NEXT_PUBLIC_DRUPAL_BASE_URL: zod.string(),
-});
-
-export const env = envSchema.parse({
-  NODE_ENV: process.env.NODE_ENV,
-  DRUPAL_CLIENT_ID: process.env.DRUPAL_CLIENT_ID,
-  DRUPAL_CLIENT_SECRET: process.env.DRUPAL_CLIENT_SECRET,
-  DRUPAL_REVALIDATE_SECRET: process.env.DRUPAL_REVALIDATE_SECRET,
-  NEXT_IMAGE_DOMAIN: process.env.NEXT_IMAGE_DOMAIN,
-  NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
-  NEXT_PUBLIC_DRUPAL_BASE_URL: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+export const env = createEnv({
+  server: {
+    DRUPAL_CLIENT_ID: zod.string(),
+    DRUPAL_CLIENT_SECRET: zod.string(),
+    DRUPAL_REVALIDATE_SECRET: zod.string(),
+    NEXT_IMAGE_DOMAIN: zod.string(),
+  },
+  client: {
+    NEXT_PUBLIC_DRUPAL_BASE_URL: zod.string().url(),
+    NEXT_PUBLIC_FRONTEND_URL: zod.string().url(),
+  },
+  runtimeEnv: {
+    DRUPAL_CLIENT_ID: process.env.DRUPAL_CLIENT_ID,
+    DRUPAL_CLIENT_SECRET: process.env.DRUPAL_CLIENT_SECRET,
+    DRUPAL_REVALIDATE_SECRET: process.env.DRUPAL_REVALIDATE_SECRET,
+    NEXT_IMAGE_DOMAIN: process.env.NEXT_IMAGE_DOMAIN,
+    NEXT_PUBLIC_DRUPAL_BASE_URL: process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
+    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+  },
 });
