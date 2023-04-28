13b5017666e47f58ec701e2ac5207fe8a0b58795
Joshua Scott <git@joshuascott.io>
2023-04-28 17:19:06 +0300

    Use env import instead of process.env

--- next/lib/drupal.ts
+++ next/lib/drupal.ts
@@ -2,13 +2,10 @@ import { DrupalClient } from "next-drupal";
 
 import { env } from "@/env";
 
-export const drupal = new DrupalClient(
-  process.env.NEXT_PUBLIC_DRUPAL_BASE_URL,
-  {
-    forceIframeSameSiteCookie: env.NODE_ENV === "development",
-    auth: {
-      clientId: env.DRUPAL_CLIENT_ID,
-      clientSecret: env.DRUPAL_CLIENT_SECRET,
-    },
-  }
-);
+export const drupal = new DrupalClient(env.NEXT_PUBLIC_DRUPAL_BASE_URL, {
+  forceIframeSameSiteCookie: env.NODE_ENV === "development",
+  auth: {
+    clientId: env.DRUPAL_CLIENT_ID,
+    clientSecret: env.DRUPAL_CLIENT_SECRET,
+  },
+});
