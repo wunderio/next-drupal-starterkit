205ce08c5c2831c8834088d300b5492ec1bdc21e
Joshua Scott <git@joshuascott.io>
2023-05-05 16:15:47 +0300

    NEX-50: Fix className merging in some UI components

--- next/wunder-component-library/input.tsx
+++ next/wunder-component-library/input.tsx
@@ -8,14 +8,14 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
     return (
       <input
-        className={cn([
+        className={cn(
           "flex w-full items-center rounded border-2 border-stone bg-white px-2 py-1.5 text-md text-steelgray",
           "placeholder:text-stone",
           "hover:enabled:border-steelgray",
           "focus:border-primary-600 active:enabled:border-primary-600",
           "disabled:cursor-not-allowed disabled:border-finnishwinter disabled:bg-finnishwinter disabled:text-finnishwinter",
-          className,
-        ])}
+          className
+        )}
         ref={ref}
         {...props}
       />
