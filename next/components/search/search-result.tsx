5ab394ab01092c01015c86f2450b4d612af9040f
Joshua Scott <git@joshuascott.io>
2023-05-08 13:29:21 +0300

    NEX-48: Do not open search results in a new tab

--- next/components/search/search-result.tsx
+++ next/components/search/search-result.tsx
@@ -6,10 +6,8 @@ export function SearchResult({ result, onClickLink }: ResultViewProps) {
   return (
     <a
       onClick={onClickLink}
-      rel="noopener noreferrer"
       key={result.id.raw}
       href={result.path.raw}
-      target="_blank"
       className="my-4 block rounded bg-white p-8"
     >
       <h5 className="mb-4 text-xl font-bold text-primary-800">
