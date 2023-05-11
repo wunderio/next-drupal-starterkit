7cf0393ec4bdf62cda25fa3286d5bb53e63c4617
David Hauser <david.hauser@wunder.io>
2023-05-11 15:37:16 +0300

    nex-64-hero: styled hero and used component lib btn styles

--- next/components/paragraph--hero.tsx
+++ next/components/paragraph--hero.tsx
@@ -6,50 +6,61 @@ import { MediaImage } from "@/components/media--image";
 import { Hero as HeroType } from "@/lib/zod/paragraph";
 import ArrowIcon from "@/styles/icons/arrow-down.svg";
 
+import { withButtonStyles } from "@/wunder-component-library/button";
+
+export const LinkButtonWithStyles = withButtonStyles<any>(Link);
+
 export function ParagraphHero({ paragraph }: { paragraph: HeroType }) {
   return (
-    <>
-      {paragraph.field_heading && (
-        <h1 className="text-left text-heading-md font-bold text-steelgray md:text-heading-lg">
-          {paragraph.field_heading}
-        </h1>
-      )}
-      <FormattedText
-        html={paragraph.field_formatted_text.processed}
-        className={clsx(
-          "text-justify text-md/xl text-scapaflow sm:text-lg",
-          paragraph.field_heading && "mt-4"
-        )}
-      />
-      <MediaImage media={paragraph.field_image} priority />
-
-      {paragraph.field_primary_link && (
-        <Link
-          role="listitem"
-          href={paragraph.field_primary_link.full_url}
-          className="relative min-h-[6em] cursor-pointer rounded border border-finnishwinter bg-primary-100 p-8 text-lg text-steelgray hover:bg-primary-200"
-        >
-          {paragraph.field_primary_link.title}
-          <ArrowIcon
-            aria-hidden
-            className="absolute right-2 top-1/4 h-6 w-6 -rotate-90 text-primary-600"
+    <section id="hero" className="bg-secondary-50">
+      <div className="mx-auto grid max-w-screen-xl lg:grid-cols-12">
+        <div className="mr-auto place-self-center px-8 py-8 lg:col-span-6  lg:py-16">
+          {paragraph.field_heading && (
+            <h1 className="leading-none mb-4 max-w-2xl text-left text-heading-md font-bold tracking-tight text-primary-600 md:text-heading-lg">
+              {paragraph.field_heading}
+            </h1>
+          )}
+          <FormattedText
+            html={paragraph.field_formatted_text.processed}
+            className={clsx(
+              "mb-6 max-w-2xl text-left text-md/xl text-primary-600 sm:text-lg md:text-lg lg:mb-8 lg:text-xl",
+              paragraph.field_heading && "mt-4"
+            )}
           />
-        </Link>
-      )}
+          <div className="gap-4 sm:text-left">
+            {paragraph.field_primary_link && (
+              <LinkButtonWithStyles
+                href={paragraph.field_primary_link.full_url}
+                className="text-base mr-4 inline-flex px-5 py-3"
+                variant="primary"
+              >
+                {paragraph.field_primary_link.title}
+                <ArrowIcon
+                  aria-hidden
+                  className="bg:white ml-3 h-6 w-6 -rotate-90"
+                />
+              </LinkButtonWithStyles>
+            )}
 
-      {paragraph.field_secondary_link && (
-        <Link
-          role="listitem"
-          href={paragraph.field_secondary_link.full_url}
-          className="relative min-h-[6em] cursor-pointer rounded border border-finnishwinter bg-primary-100 p-8 text-lg text-steelgray hover:bg-primary-200"
-        >
-          {paragraph.field_secondary_link.title}
-          <ArrowIcon
-            aria-hidden
-            className="absolute right-2 top-1/4 h-6 w-6 -rotate-90 text-primary-600"
-          />
-        </Link>
-      )}
-    </>
+            {paragraph.field_secondary_link && (
+              <LinkButtonWithStyles
+                href={paragraph.field_secondary_link.full_url}
+                className="text-base mt-3 inline-flex px-5 py-3 sm:mt-0"
+                variant="secondary"
+              >
+                {paragraph.field_secondary_link.title}
+                <ArrowIcon
+                  aria-hidden
+                  className="bg:white ml-3 h-6 w-6 -rotate-90"
+                />
+              </LinkButtonWithStyles>
+            )}
+          </div>
+        </div>
+        <div className="hidden lg:col-span-6 lg:mt-0 lg:flex">
+          <MediaImage media={paragraph.field_image} priority />
+        </div>
+      </div>
+    </section>
   );
 }
