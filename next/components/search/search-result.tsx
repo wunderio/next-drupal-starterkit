9e43140d14c27084dd81fde7579d099e6260feb4
David Hauser <david.hauser@wunder.io>
2023-05-17 08:46:48 +0300

    nex-68-searcha11y: added li wrapper for search results

--- next/components/search/search-result.tsx
+++ next/components/search/search-result.tsx
@@ -4,18 +4,16 @@ import { Badge } from "@/wunder-component-library/badge";
 
 export function SearchResult({ result }: ResultViewProps) {
   return (
-    <a
-      key={result.id.raw}
-      href={result.path.raw}
-      className="my-4 block rounded bg-white p-8"
-    >
-      <h2 className="mb-4 text-xl font-bold text-primary-800">
-        {result.title.raw}
-      </h2>
-      <p className="mb-6 text-md text-scapaflow">{result.excerpt.raw}</p>
-      <Badge variant="info" size="sm">
-        {result.content_type.raw}
-      </Badge>
-    </a>
+    <li key={result.id.raw} className="my-4 block rounded bg-white p-8">
+      <a href={result.path.raw}>
+        <h2 className="mb-4 text-xl font-bold text-primary-800">
+          {result.title.raw}
+        </h2>
+        <p className="mb-6 text-md text-scapaflow">{result.excerpt.raw}</p>
+        <Badge variant="info" size="sm">
+          {result.content_type.raw}
+        </Badge>
+      </a>
+    </li>
   );
 }
