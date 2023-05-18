39977bc91669267ab08e77ea34768fc9598cb383
Joshua Scott <git@joshuascott.io>
2023-05-18 14:36:24 +0300

    NEX-50: Add Modal component

---
+++ next/stories/modal.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "@/wunder-component-library/button";
+import {
+  Modal,
+  ModalContent,
+  ModalDescription,
+  ModalHeader,
+  ModalTitle,
+  ModalTrigger,
+} from "@/wunder-component-library/modal";
+
+const meta: Meta<typeof Modal> = {
+  title: "Library/Modal",
+  component: Modal,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+export const Default: Story = {
+  render: () => (
+    <Modal>
+      <ModalTrigger asChild>
+        <Button>Open modal</Button>
+      </ModalTrigger>
+      <ModalContent>
+        <ModalHeader>
+          <ModalTitle>Modal title</ModalTitle>
+          <ModalDescription>Optional modal description.</ModalDescription>
+        </ModalHeader>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu
+          turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec
+          fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus
+          elit sed risus. Maecenas eget condimentum velit, sit amet feugiat
+          lectus. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Praesent auctor purus luctus enim
+          egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex.
+          Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum
+          lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in
+          elementum tellus.
+        </p>
+      </ModalContent>
+    </Modal>
+  ),
+};
