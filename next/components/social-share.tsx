8f7fb4fd593b0dd9f194a2e661c9441f7f4c3d65
David Hauser <david.hauser@wunder.io>
2023-05-23 09:24:44 +0300

    nex-72: added translations and refactored

--- next/components/social-share.tsx
+++ next/components/social-share.tsx
@@ -8,51 +8,53 @@ import TwitterIcon from "../styles/icons/twitter.svg";
 export function SocialShare() {
   const [pageUrl, setPageUrl] = useState<string>("");
   const { t } = useTranslation();
+
   useEffect(() => {
     const currentUrl = window.location.href;
     setPageUrl(currentUrl);
   }, [pageUrl]);
 
+  const data = [
+    {
+      id: 1,
+      location: "Facebook",
+      url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+        pageUrl
+      )}`,
+      icon: <FacebookIcon className="block h-16 w-16 text-primary-600" />,
+    },
+    {
+      id: 2,
+      location: "Twitter",
+      url: `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+        pageUrl
+      )}`,
+      icon: <TwitterIcon className="block h-16 w-16 text-primary-600" />,
+    },
+    {
+      id: 3,
+      location: "LinkedIn",
+      url: `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
+        pageUrl
+      )}`,
+      icon: <LinkedInIcon className="block h-16 w-16 text-primary-600" />,
+    },
+  ];
+
   return (
     <div>
       <p className="text-center">{t("share-page")}</p>
       <ul className="flex flex-wrap justify-center">
-        <li className="m-4">
-          <a
-            href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-              pageUrl
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FacebookIcon className="inline-block h-16 w-16 text-primary-600" />
-            <span className="sr-only">{`Share to Facebook`}</span>
-          </a>
-        </li>
-        <li className="m-4">
-          <a
-            href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
-              pageUrl
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <TwitterIcon className="inline-block h-16 w-16 text-primary-600" />
-            <span className="sr-only">{`Share to Twitter`}</span>
-          </a>
-        </li>
-        <li className="m-4">
-          <a
-            href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
-              pageUrl
-            )}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <LinkedInIcon className="inline-block h-16 w-16 text-primary-600" />
-            <span className="sr-only">{`Share to LinkedIn`}</span>
-          </a>
-        </li>
+        {data?.map((link) => (
+          <li className="m-4" key={link.id}>
+            <a href={link.url} target="_blank" rel="noopener noreferrer">
+              {link.icon}
+              <span className="sr-only">{`${t("share-to")} ${
+                link.location
+              }`}</span>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
