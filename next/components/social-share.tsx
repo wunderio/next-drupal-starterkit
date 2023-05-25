c356c66184936ca47bc17b55b6cce9ed3fc0711e
David Hauser <david.hauser@wunder.io>
2023-05-25 09:53:08 +0300

    nex-72: fixed translation and refactored code

--- next/components/social-share.tsx
+++ next/components/social-share.tsx
@@ -1,42 +1,36 @@
 import { useTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
 
-import FacebookIcon from "../styles/icons/facebook.svg";
-import LinkedInIcon from "../styles/icons/linkedin.svg";
-import TwitterIcon from "../styles/icons/twitter.svg";
+import FacebookIcon from "@/styles/icons/facebook.svg";
+import LinkedInIcon from "@/styles/icons/linkedin.svg";
+import TwitterIcon from "@/styles/icons/twitter.svg";
 
 export function SocialShare() {
   const [pageUrl, setPageUrl] = useState<string>("");
   const { t } = useTranslation();
 
   useEffect(() => {
-    const currentUrl = window.location.href;
+    const currentUrl = encodeURIComponent(window.location.href);
     setPageUrl(currentUrl);
-  }, [pageUrl]);
+  }, []);
 
   const data = [
     {
       id: 1,
       location: "Facebook",
-      url: `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-        pageUrl
-      )}`,
+      url: `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`,
       icon: <FacebookIcon className="block h-16 w-16 text-primary-600" />,
     },
     {
       id: 2,
       location: "Twitter",
-      url: `https://twitter.com/intent/tweet?url=${encodeURIComponent(
-        pageUrl
-      )}`,
+      url: `https://twitter.com/intent/tweet?url=${pageUrl}`,
       icon: <TwitterIcon className="block h-16 w-16 text-primary-600" />,
     },
     {
       id: 3,
       location: "LinkedIn",
-      url: `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
-        pageUrl
-      )}`,
+      url: `https://www.linkedin.com/shareArticle?mini=true&url=${pageUrl}`,
       icon: <LinkedInIcon className="block h-16 w-16 text-primary-600" />,
     },
   ];
@@ -45,13 +39,17 @@ export function SocialShare() {
     <div>
       <p className="text-center">{t("share-page")}</p>
       <ul className="flex flex-wrap justify-center">
-        {data?.map((link) => (
-          <li className="m-4" key={link.id}>
-            <a href={link.url} target="_blank" rel="noopener noreferrer">
-              {link.icon}
-              <span className="sr-only">{`${t("share-to")} ${
-                link.location
-              }`}</span>
+        {data?.map(({ id, url, icon, location }) => (
+          <li className="m-4" key={id}>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {icon}
+              {location && (
+                <span className="sr-only">
+                  {t("share-to", {
+                    location,
+                  })}
+                </span>
+              )}
             </a>
           </li>
         ))}
