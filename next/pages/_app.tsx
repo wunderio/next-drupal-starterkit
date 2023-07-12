fb66d7dbbd4568f82a37d008a5f850d923af433b
Joshua Scott <git@joshuascott.io>
2023-07-12 16:44:08 +0300

    Move font declarations to style folder

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,13 +1,11 @@
 import "@/styles/globals.css";
 
 import { AppProps } from "next/app";
-import { Inter, Overpass } from "next/font/google";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from "next-i18next";
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import clsx from "clsx";
 
 import { Layout } from "@/components/layout";
 import {
@@ -15,6 +13,7 @@ import {
   LanguageLinksProvider,
 } from "@/lib/contexts/language-links-context";
 import { CommonPageProps } from "@/lib/get-common-page-props";
+import { inter, overpass } from "@/styles/fonts";
 
 interface PageProps extends CommonPageProps {
   languageLinks?: LanguageLinks;
@@ -39,25 +38,9 @@ function App({ Component, pageProps }: AppProps<PageProps>) {
   );
 }
 
-// Add fonts (see https://nextjs.org/docs/basic-features/font-optimization#google-fonts)
-// Note that fonts must be created in _app.tsx to be available on every page.
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-const overpass = Overpass({
-  subsets: ["latin"],
-  variable: "--font-overpass",
-});
-const fontVariables = [inter, overpass].map((font) => font.variable);
 function Fonts({ children }: { children: React.ReactNode }) {
   return (
-    <div
-      className={clsx(
-        fontVariables, // Add font variables so they can be used in CSS
-        "font-overpass", // Set default font
-      )}
-    >
+    <div className={`${inter.variable} ${overpass.variable} font-overpass`}>
       {children}
     </div>
   );
