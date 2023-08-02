5874743b4a0c878549aa34a4df64bf5033068463
Joshua Scott <git@joshuascott.io>
2023-08-02 15:11:11 +0300

    NEX-17: Fix lint errors

--- next/cypress/e2e/template.cy.ts
+++ next/cypress/e2e/template.cy.ts
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 
-// @ts-ignore
 describe("Frontend test for Login and Sending Message", () => {
   it("Logs in and is able to fill the webform", () => {
     cy.visit("https://frontend.lndo.site/auth/login?callbackUrl=%2Fen%2F");
@@ -15,52 +14,52 @@ describe("Frontend test for Login and Sending Message", () => {
   });
 });
 
-describe('Frontend Test for Search Functionality', () => {
-  it('Opens the page, performs search, and verifies search results', () => {
+describe("Frontend Test for Search Functionality", () => {
+  it("Opens the page, performs search, and verifies search results", () => {
     // Navigating to the main page and clicking on Search button
-    cy.visit("https://frontend.lndo.site"); 
-    cy.get('a[href="/search"]').click()
+    cy.visit("https://frontend.lndo.site");
+    cy.get('a[href="/search"]').click();
     cy.wait(500);
     // Searching for string "Drupal"
-    cy.get("[data-transaction-name='search input']").type('Drupal');
+    cy.get("[data-transaction-name='search input']").type("Drupal");
     cy.get("[data-transaction-name='search submit']").click();
     // Waiting for the search results to load
     cy.wait(1000);
     // Verifying results
-    cy.get(".sui-results-container").should('be.visible');
-    cy.contains('Drupal and Next.js can be used together').should('be.visible');
+    cy.get(".sui-results-container").should("be.visible");
+    cy.contains("Drupal and Next.js can be used together").should("be.visible");
   });
 });
 
-describe('Frontend Test for Language Switching', () => {
-  it('Opens the page, switches language and verifies it', () => {
+describe("Frontend Test for Language Switching", () => {
+  it("Opens the page, switches language and verifies it", () => {
     // Navigating to the main page and clicking on Language button
-    cy.visit("https://frontend.lndo.site"); 
-    cy.contains('English').click({ force: true });
+    cy.visit("https://frontend.lndo.site");
+    cy.contains("English").click({ force: true });
     cy.wait(500);
     // Clicking through the language options and checking translations
-    cy.contains('Suomi').click({ force: true });
+    cy.contains("Suomi").click({ force: true });
     cy.wait(500);
-    cy.contains('Monikielinen ja mielikuvituksellinen kokoonpano').should('be.visible');
-    cy.contains('Suomi').click({ force: true });
+    cy.contains("Monikielinen ja mielikuvituksellinen kokoonpano").should(
+      "be.visible"
+    );
+    cy.contains("Suomi").click({ force: true });
     cy.wait(500);
-    cy.contains('Svenska').click({ force: true });
+    cy.contains("Svenska").click({ force: true });
     cy.wait(500);
-    cy.contains('En flerspråkig och egensinnig installation').should('be.visible');
+    cy.contains("En flerspråkig och egensinnig installation").should(
+      "be.visible"
+    );
   });
 });
 
-describe('Basic Layout Test', () => {
-  it('Opens the page, checks if header and footer is in place', () => {
+describe("Basic Layout Test", () => {
+  it("Opens the page, checks if header and footer is in place", () => {
     // Navigating to the main page and checking if header and footer exists
     cy.visit("https://frontend.lndo.site");
     // Checking if header exists
-    cy.get('header')
-      .should('be.visible')
-      .should('exist');
+    cy.get("header").should("be.visible").should("exist");
     // Checking if footer exists
-    cy.get('footer')
-      .should('be.visible')
-      .should('exist');
+    cy.get("footer").should("be.visible").should("exist");
   });
 });
