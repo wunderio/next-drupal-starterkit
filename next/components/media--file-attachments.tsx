7b4dc3d7353de23e3a85724a6e4a433329443dc0
David Hauser <david.hauser@wunder.io>
2023-08-21 15:58:08 +0300

    NEX-18: added two svgs, made silt yml config changes, and have component from nemo ready for refactoring

--- next/components/media--file-attachments.tsx
+++ next/components/media--file-attachments.tsx
@@ -1,6 +1,9 @@
 import { absoluteUrl } from "@/lib/drupal/absolute-url";
+import { formatFileSizeInBytes, getFileType } from "@/lib/utils";
 import { FileAttachments } from "@/lib/zod/paragraph";
 import ListIcon from "@/styles/icons/list.svg";
+// import PdfIcon from "@/styles/icons/pdf.svg";
+// import TextIcon from "@/styles/icons/text-doc.svg";
 
 interface MediaFileAttachmentsProps {
   mediaItems: FileAttachments["field_file_attachments"];
@@ -44,25 +47,90 @@ export function MediaFileAttachments({
   );
 }
 
-function formatFileSizeInBytes(x?: number) {
-  const units = [
-    "bytes",
-    "KiB",
-    "MiB",
-    "GiB",
-    "TiB",
-    "PiB",
-    "EiB",
-    "ZiB",
-    "YiB",
-  ];
+/*
+interface MediaFileAttachmentsProps {
+  media: FileAttachments["field_file_attachments"];
+  className?: string;
+}
 
-  let l = 0,
-    n = parseInt(String(x), 10) || 0;
-
-  while (n >= 1024 && ++l) {
-    n = n / 1024;
+export function MediaFileAttachments({
+  media,
+  className,
+  ...props
+}: MediaFileAttachmentsProps) {
+  if (media.length === 0) {
+    return null;
   }
 
-  return n.toFixed(n < 10 && l > 0 ? 1 : 0) + " " + units[l];
+  const getIcon = (fileType: string) => {
+    switch (fileType) {
+      case "pdf":
+        return (
+          <PdfIcon
+            className="mb-2 flex h-12 w-12 flex-shrink-0 sm:mb-0 sm:mr-8"
+            xmlns="http://www.w3.org/2000/svg"
+          />
+        );
+      case "docx":
+      case "txt":
+      case "odt":
+      case "rtf":
+        return (
+          <TextIcon
+            className="mb-2 flex h-12 w-12 flex-shrink-0 sm:mb-0 sm:mr-8"
+            xmlns="http://www.w3.org/2000/svg"
+          />
+        );
+      default:
+        return (
+          <FileIcon
+            className="mb-2 flex h-12 w-12  flex-shrink-0 sm:mb-0 sm:mr-8"
+            xmlns="http://www.w3.org/2000/svg"
+          />
+        );
+    }
+  };
+
+  return (
+    <Container>
+      <ul
+        className={clsx(
+          "w-full list-inside space-y-4 text-body-md text-black",
+          className,
+        )}
+        {...props}
+      >
+        {media.map((mediaItem) => (
+          <li
+            key={mediaItem.id}
+            className="flex min-h-[80px] items-center border-2 border-brand-10 hover:bg-brand-10"
+          >
+            <a
+              href={absoluteUrlFastly(mediaItem.field_media_document.uri.url)}
+              className="flex w-full flex-col items-center justify-between p-4 align-middle sm:flex-row sm:px-8 sm:py-4"
+            >
+              <div className="flex w-full flex-col items-center sm:flex-row">
+                {getIcon(getFileType(mediaItem.field_media_document.uri.url))}
+                <p className="mb-2 text-center text-body-sm sm:mb-0 sm:text-left sm:text-body-md">
+                  {mediaItem.field_media_document.filename}{" "}
+                  <span>
+                    ({getFileType(mediaItem.field_media_document.uri.url)},{" "}
+                    {formatFileSizeInBytes(
+                      mediaItem.field_media_document.filesize,
+                    )}
+                    )
+                  </span>
+                </p>
+              </div>
+              <DownloadIcon
+                className="mr-0 h-6 w-6 flex-shrink-0 sm:ml-4"
+                xmlns="http://www.w3.org/2000/svg"
+              />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </Container>
+  );
 }
+*/
