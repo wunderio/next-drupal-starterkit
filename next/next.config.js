875be031499e82ffffd7891244a6552f4c28d1f6
Joshua Scott <git@joshuascott.io>
2023-12-07 16:33:27 +0200

    NEX-127: Fix svg loader See https://github.com/vercel/next.js/issues/48177#issuecomment-1506251112

--- next/next.config.js
+++ next/next.config.js
@@ -19,7 +19,6 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
-      issuer: /\.tsx$/i,
       loader: "@svgr/webpack",
     });
     return config;
