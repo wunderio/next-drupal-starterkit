44e53f1f1b3584859c9f1a6e24eac7bc0dd0fb14
Mario Vercellotti <mario.vercellotti@wunder.io>
2024-02-01 12:08:47 +0200

    NEX-15: Get articleTeaserType by extracting by typename

--- next/types/graphql.ts
+++ next/types/graphql.ts
@@ -35,17 +35,11 @@ export type MenuType = GetMenuQuery["menu"];
 /**
  * We have a representation of an article that we get from the ArticleListingQuery,
  * and we want to get its type to use it in the ArticleTeasers and ArticleTeaser components.
- *
- * The "results" property of the query will return a union of all possible defined
- * nodeTypes, but we know, because of filters in the view and the fragment we have defined,
- * that only NodeArticles will be returned. For all other node types, the type will only
- * have the __typename property, while for article it will have typename and other properties,
- * including "id".
- *
- * So, we can extract the type of NodeArticle from the union, by looking for the type that can have the id property.
+ * So, we can extract the type of NodeArticle from the union by looking at the __typename
+ * property.
  *
  */
 export type ArticleTeaserType = Extract<
   ArticleListingQuery["articlesView"]["results"][number],
-  { id: string }
+  { __typename: "NodeArticle" }
 >;
