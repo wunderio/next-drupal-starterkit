ee0b11a0c628ae2fdc220bd7e3570b999002be19
Joshua Scott <git@joshuascott.io>
2024-05-21 17:13:28 +0300

    NEX-153: Update graphql-codegen config to authenticate via access token

--- next/codegen.ts
+++ next/codegen.ts
@@ -1,8 +1,27 @@
-import { CodegenConfig } from "@graphql-codegen/cli";
+import { loadEnvConfig } from "@next/env";
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+const tokenFilePath = path.resolve(os.tmpdir(), ".drupal-access-token.txt");
+const token = fs.readFileSync(tokenFilePath, "utf8").trim();
+
+loadEnvConfig(process.cwd());
+
+/* eslint-disable-next-line n/no-process-env */
+const schemaUrl = `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/graphql`;
 
 const config: CodegenConfig = {
-  // eslint-disable-next-line n/no-process-env
-  schema: `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/graphql`,
+  schema: [
+    {
+      [schemaUrl]: {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      },
+    },
+  ],
   documents: ["lib/graphql/**/*.ts", "!./node_modules/**/*", "!./.next/**/*"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
