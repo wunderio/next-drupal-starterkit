9dbb150a47c584c5fb7e6003bec94dc0e0fbd076
Mario Vercellotti <mario.vercellotti@wunder.io>
2024-06-06 16:00:21 +0300

    NEX-133: Adjust the graphql schema and query to allow to display liftup_articles paragraphs on the frontpage

--- next/lib/graphql/fragments/nodes.ts
+++ next/lib/graphql/fragments/nodes.ts
@@ -65,6 +65,7 @@ export const FRAGMENT_NODE_FRONTPAGE = graphql(`
           ...FragmentParagraphHero
           ...FragmentParagraphAccordion
           ...FragmentParagraphListingArticle
+          ...FragmentParagraphLiftupArticle
         }
       }
     }
