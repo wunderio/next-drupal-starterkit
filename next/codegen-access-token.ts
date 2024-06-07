3db3b15d91e0f4251bc3a9a54f59b88090bd169f
Joshua Scott <git@joshuascott.io>
2024-06-07 13:04:58 +0300

    Move require statements inside IIFE This avoids polluting the global namespace, which will result in an error if another script assigns require statements to the same const.

--- next/codegen-access-token.ts
+++ next/codegen-access-token.ts
@@ -1,18 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const { loadEnvConfig } = require("@next/env");
-const fs = require("fs");
-const path = require("path");
-const os = require("os");
-
-loadEnvConfig(process.cwd());
-
 /**
  * Gets an access token from Drupal and saves it the temporary directory,
  * so it can be used by graphql-codegen to authenticate to the Drupal GraphQL API.
  */
 void (async function getAndSaveAccessToken() {
   try {
+    /* eslint-disable @typescript-eslint/no-var-requires */
+    const { loadEnvConfig } = require("@next/env");
+    const fs = require("fs");
+    const path = require("path");
+    const os = require("os");
+    /* eslint-enable @typescript-eslint/no-var-requires */
+
+    loadEnvConfig(process.cwd());
+
     /* eslint-disable n/no-process-env */
     const oauthUrl = `${process.env.NEXT_PUBLIC_DRUPAL_BASE_URL}/oauth/token`;
     const credentials = Buffer.from(
