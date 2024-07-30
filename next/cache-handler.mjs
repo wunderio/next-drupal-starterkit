21eea568de5c3b1efd9809dae8a1357e0e7e9757
Mario Vercellotti <mario.vercellotti@wunder.io>
2024-07-30 15:15:04 +0300

    NEX-104: Set long stale times for redis so that data don't disappear too quickly for next.js to use it

--- next/cache-handler.mjs
+++ next/cache-handler.mjs
@@ -1,7 +1,7 @@
-import { CacheHandler } from '@neshca/cache-handler';
-import createLruHandler from '@neshca/cache-handler/local-lru';
-import createRedisHandler from '@neshca/cache-handler/redis-strings';
-import { createClient } from 'redis';
+import { CacheHandler } from "@neshca/cache-handler";
+import createLruHandler from "@neshca/cache-handler/local-lru";
+import createRedisHandler from "@neshca/cache-handler/redis-strings";
+import { createClient } from "redis";
 
 CacheHandler.onCreation(async () => {
   let redisHandler;
@@ -25,7 +25,7 @@ CacheHandler.onCreation(async () => {
     redisHandler = await createRedisHandler({
       client,
       keyPrefix: `nextjs-cache:`,
-      sharedTagsKey: '_sharedTags_',
+      sharedTagsKey: "_sharedTags_",
       // timeout for the Redis client operations like `get` and `set`
       // after this timeout, the operation will be considered failed and the `localHandler` will be used
       timeoutMs: 2000,
@@ -34,8 +34,16 @@ CacheHandler.onCreation(async () => {
 
   const localHandler = createLruHandler();
 
+  // We set this to a high value so redis will not automatically delete data on expiration, but keep it
+  // as stale for a long period. This way, Next will be able to use it for its stale-while-revalidate logic.
+  const defaultStaleAge = 3600 * 24 * 7; // 1 week
+
   return {
     handlers: [redisHandler, localHandler],
+    ttl: {
+      defaultStaleAge,
+      estimateExpireAge: () => defaultStaleAge,
+    },
   };
 });
 
