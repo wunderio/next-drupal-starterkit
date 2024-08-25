b6a80b8b9ccde21c1ea92c37f3b7907db88fa61c
Jesse Manninen <jesse@mobile-user-c2599c-118.dhcp.inet.fi>
2024-08-25 23:49:18 +0300

    NEX-127-1: /app folder to tailwind.config

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./{components,lib,pages,stories,styles,ui}/**/*"],
+  content: ["./{components,lib,app,pages,stories,styles,ui}/**/*"],
   theme: {
     colors: {
       transparent: "transparent",
