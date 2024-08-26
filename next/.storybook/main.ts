ea3bccd97ff381b7bc30d8d6fe59f02e59359719
Jesse Manninen <jesse@mobile-user-c2599c-118.dhcp.inet.fi>
2024-08-26 23:37:33 +0300

    NEX-127-1: Storybook imports fix

--- next/.storybook/main.ts
+++ next/.storybook/main.ts
@@ -1,7 +1,10 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../stories/**/*.mdx", "../stories/*.stories.@(js|jsx|ts|tsx)"],
+  stories: [
+    "../src/stories/**/*.mdx",
+    "../src/stories/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
