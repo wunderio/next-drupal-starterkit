5f6de7b4f532ed35b73dd63bec366ebd298151be
Jesse Manninen <jesse@Jesses-MacBook-Pro.local>
2024-08-28 22:50:58 +0300

    NEX-127-1: Add redirect to middleware if authenticated user tries to access login / register pages

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -5,10 +5,22 @@ import { auth } from "./auth";
 import { routing } from "./routing";
 
 const DEFAULT_LOGIN_URL = "/auth/login";
+
+// Auth routes & protected routes
+// Add more routes as needed.
+// For now need to add the internationalized routes too 😁
 const PROTECTED_ROUTES = [
   "/dashboard",
   "/fi/hallintapaneeli",
   "/sv/instrumentpanel",
+];
+const AUTH_ROUTES = [
+  "/login",
+  "/register",
+  "/fi/kirjaudu",
+  "/fi/rekisteröidy",
+  "/sv/logga-in",
+  "/sv/registrera",
 ];
 
 interface AppRouteHandlerFnContext {
@@ -22,11 +34,15 @@ const intlMiddleware = createMiddleware(routing, {
 const authMiddleware = (request: NextRequest, ctx: AppRouteHandlerFnContext) =>
   auth((req) => {
     const isLoggedIn = req.auth?.user;
-
     const isProtectedRoute = PROTECTED_ROUTES.some((route) =>
       req.nextUrl.pathname.startsWith(route),
     );
+    const isAuthRoute = AUTH_ROUTES.some((route) =>
+      req.nextUrl.pathname.startsWith(route),
+    );
 
+    // Redirect to default login page with requested URL as callbackUrl if accessing a protected route
+    // without being logged in
     if (isProtectedRoute) {
       if (!isLoggedIn) {
         return NextResponse.redirect(
@@ -35,6 +51,13 @@ const authMiddleware = (request: NextRequest, ctx: AppRouteHandlerFnContext) =>
             req.nextUrl,
           ),
         );
+      }
+    }
+
+    // Redirect to frontpage if accessing an auth route while being logged in
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return NextResponse.redirect(new URL("/", req.nextUrl));
       }
     }
 
