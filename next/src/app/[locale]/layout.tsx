dfdb83fd6884c9b486b8654e8de5bcee97597499
Jesse Manninen <jesse@Jesses-MacBook-Pro.local>
2024-09-03 03:51:29 +0300

    NEX-127-1: Add draft alert component to root layout

--- next/src/app/[locale]/layout.tsx
+++ next/src/app/[locale]/layout.tsx
@@ -1,15 +1,16 @@
 import "@/styles/globals.css";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Viewport } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, unstable_setRequestLocale } from "next-intl/server";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import AuthProvider from "@/components/auth-provider";
 import { Footer } from "@/components/footer/footer";
 import ReactQueryClientProvider from "@/components/query-client-provider";
 import { inter, overpass } from "@/styles/fonts";
 
+import DraftAlert from "@/components/draft-alert";
 import { routing } from "@/routing";
 
 export function generateStaticParams() {
@@ -42,6 +43,7 @@ export default async function RootLayout({
           <NextIntlClientProvider messages={messages}>
             <ReactQueryClientProvider>
               <Fonts>
+                <DraftAlert />
                 <div className="flex flex-col min-h-screen">
                   {children}
                   <Footer />
