64d54265bff433c9618998a4a84b8ffdcf36eb7b
Jesse Manninen <jesse@mobile-user-c2599c-118.dhcp.inet.fi>
2024-09-16 01:11:49 +0300

    NEX-127: REVALIDATE_LONG to 600

--- next/src/lib/constants.ts
+++ next/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const REVALIDATE_SHORT = 10; // 10 seconds
 
 // If you use Redis caching and you change this value, you should also change the constant with the same name in the cache handler.
-export const REVALIDATE_LONG = 60 * 10; // 10 minutes
+export const REVALIDATE_LONG = 600; // 10 minutes
