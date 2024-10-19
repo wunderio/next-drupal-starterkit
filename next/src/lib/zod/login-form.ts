8327bdf89ea915bec48edcc397ff1c4a05d7745e
Jesse Manninen <jesse@Jesses-MacBook-Pro.local>
2024-10-19 17:06:58 +0300

    NEX-127: Zod schemas for login/register forms

---
+++ next/src/lib/zod/login-form.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const loginFormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
+
+export type LoginFormInputs = z.infer<typeof loginFormSchema>;
