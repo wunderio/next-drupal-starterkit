6ac436c09364d2fc12e9a85658a4d50e24cb4630
Mario Vercellotti <mario.vercellotti@wunder.io>
2024-10-23 15:18:51 +0300

    NEX-127: Remove unused import

--- next/src/components/forms/contact-form.tsx
+++ next/src/components/forms/contact-form.tsx
@@ -4,7 +4,6 @@ import { useTranslations } from "next-intl";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { toast } from "sonner";
-import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
