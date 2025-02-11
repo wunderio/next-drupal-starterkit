cf4e0f634b1f1de9991ea93fac46fc2fa0301cb3
Mario Vercellotti <mario.vercellotti@wunder.io>
2025-02-11 15:44:47 +0200

    NEX-184: Modify the methods to get a node to allow requesting a specific revision

--- next/src/lib/drupal/get-node.ts
+++ next/src/lib/drupal/get-node.ts
@@ -16,6 +16,7 @@ import { env } from "@/env";
  *
  * @param path The path of the node.
  * @param locale The locale of the node.
+ * @param revision The revision of the node.
  * @param isDraftMode If true, fetches the draft version of the node.
  * @returns The fetched node data or null if not found.
  */
@@ -23,11 +24,13 @@ export async function fetchNodeByPathQuery(
   path: string,
   locale: string,
   isDraftMode: boolean,
+  revision: string = null,
 ) {
   const drupalClient = isDraftMode ? drupalClientPreviewer : drupalClientViewer;
   return await drupalClient.doGraphQlRequest(GET_ENTITY_AT_DRUPAL_PATH, {
     path,
     langcode: locale,
+    revision,
   });
 }
 
@@ -40,6 +43,7 @@ const cachedFetchNodeByPathQuery = neshCache(cache(fetchNodeByPathQuery));
  * @param path The Drupal path of the node.
  * @param locale The language code for the locale of the node.
  * @param isDraftMode Optional. Defaults to false. If true, fetches the draft version of the node.
+ * @param revision Optional. The revision of the node.
  * @returns An object containing the node data or an error message.
  *
  * @example
@@ -47,11 +51,15 @@ const cachedFetchNodeByPathQuery = neshCache(cache(fetchNodeByPathQuery));
  *
  * // With draft mode enabled:
  * const { data, error } = await getNodeByPathQuery('/about-us', 'en', true);
+ *
+ * // With draft mode enabled and a specific revision:
+ * const { data, error } = await getNodeByPathQuery('/about-us', 'en', true, 'working_copy');
  */
 export async function getNodeByPathQuery(
   path: string,
   locale: string,
   isDraftMode: boolean = false,
+  revision: string = null,
 ) {
   try {
     return await cachedFetchNodeByPathQuery(
@@ -59,6 +67,7 @@ export async function getNodeByPathQuery(
       path,
       locale,
       isDraftMode,
+      revision,
     );
   } catch (error) {
     const type =
